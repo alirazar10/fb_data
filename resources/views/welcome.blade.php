503ac618c897b4d94ad5970bc3ddcd6b6e4413a5
Ali reza <aliraza.r10@gmail.com>
2021-04-08 12:15:36 +0430

    description metadata

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="twitter:description" content=" To find if your Facebook account has been hacked in a recent data breach Facebook, please search with your mobile number which is used in your Facebook account">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>FB DATA</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
